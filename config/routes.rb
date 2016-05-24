1dc108898d1134c5ca834da647b6b483776ff31c
José Tomás Albornoz <jojo@eljojo.net>
2016-05-24 12:13:47 -0400

    redirect

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     end
   end
 
-  root to: redirect("/restaurants")
+  root to: redirect("/restaurants.json")
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
